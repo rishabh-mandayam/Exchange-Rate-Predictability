97a285ede59b0ec0e8d7a0f1a4f19adf1a88c872
rishabh.mandayam@gmail.com <141846398+rishabh-mandayam@users.noreply.github.com>
2023-12-21 12:19:16 -0500

    Adjusted for seasonality and built japan

--- us_japan_old.R
+++ us_japan_old.R
@@ -3,44 +3,44 @@ library(scales)
 library(dplyr)
 
 LAMBDA = 1
-canada_time_series$e = log(canada_time_series$e)
+japan_time_series$e = log(japan_time_series$e)
 
-canada_time_series$log_us_m = log(canada_time_series$us_money_stock)
-canada_time_series$log_can_m = log(canada_time_series$can_money_stock)
+japan_time_series$log_us_m = log(japan_time_series$us_money_stock)
+japan_time_series$log_jpn_m = log(japan_time_series$jpn_money_stock)
 
-canada_time_series$log_us_y = log(canada_time_series$us_real_income)
-canada_time_series$log_can_y = log(canada_time_series$can_real_income)
+japan_time_series$log_us_y = log(japan_time_series$us_real_income)
+japan_time_series$log_jpn_y = log(japan_time_series$jpn_real_income)
 
 #Seasonality Reduction:
-canada_time_series = canada_time_series %>%
+japan_time_series = japan_time_series %>%
   mutate(
     log_us_m_lag1 = lag(log_us_m, 1),
     log_us_m_lag2 = lag(log_us_m, 2),
     log_us_m_lag3 = lag(log_us_m, 3),
-    log_can_m_lag1 = lag(log_can_m, 1),
-    log_can_m_lag2 = lag(log_can_m, 2),
-    log_can_m_lag3 = lag(log_can_m, 3),
+    log_jpn_m_lag1 = lag(log_jpn_m, 1),
+    log_jpn_m_lag2 = lag(log_jpn_m, 2),
+    log_jpn_m_lag3 = lag(log_jpn_m, 3),
   ) %>%
   mutate_all(funs(ifelse(is.na(.), 0, .)))
 
-canada_time_series = canada_time_series %>%
+japan_time_series = japan_time_series %>%
   mutate(log_us_m_lag = log_us_m + log_us_m_lag1 + log_us_m_lag2 + log_us_m_lag3)
 
-canada_time_series = canada_time_series %>%
-  mutate(log_can_m_lag = log_can_m + log_can_m_lag1 + log_can_m_lag2 + log_can_m_lag3)
+japan_time_series = japan_time_series %>%
+  mutate(log_jpn_m_lag = log_jpn_m + log_jpn_m_lag1 + log_jpn_m_lag2 + log_jpn_m_lag3)
 
-canada_time_series = canada_time_series %>%
+japan_time_series = japan_time_series %>%
   select(-log_us_m_lag1, -log_us_m_lag2, -log_us_m_lag3)
-canada_time_series = canada_time_series %>%
-  select(-log_can_m_lag1, -log_can_m_lag2, -log_can_m_lag3)
+japan_time_series = japan_time_series %>%
+  select(-log_jpn_m_lag1, -log_jpn_m_lag2, -log_jpn_m_lag3)
 
 
-m_dif = canada_time_series$log_us_m_lag - canada_time_series$log_can_m_lag
-y_dif = canada_time_series$log_us_y - canada_time_series$log_can_y
+m_dif = japan_time_series$log_us_m_lag - japan_time_series$log_jpn_m_lag
+y_dif = japan_time_series$log_us_y - japan_time_series$log_jpn_y
 
 f = m_dif - LAMBDA*y_dif
-z = f - canada_time_series$e
-one_period_lag = diff(canada_time_series$e, lag = 1)
+z = f - japan_time_series$e
+one_period_lag = diff(japan_time_series$e, lag = 1)
 
 #drop last values of f and z
 f = head(f, -1)
@@ -107,7 +107,7 @@ alpha = coef(in_sample_ols)[1]
 out_z = z[34:74]
 
 e_t_predictions = numeric(41)
-e_t_predictions[1] = canada_time_series$e[34]
+e_t_predictions[1] = japan_time_series$e[34]
 for (i in 2:40) {
   print(i)
   print(out_z[i-1])
@@ -119,16 +119,17 @@ for (i in 2:40) {
 }
 
 #plotting predictions against actual
-e_t_out_of_sample_actual = canada_time_series$e[34:74]
-time = canada_time_series$time[34:74]
+e_t_out_of_sample_actual = japan_time_series$e[34:74]
+time = japan_time_series$time[34:74]
 
 difference = (e_t_predictions - e_t_out_of_sample_actual)/(e_t_out_of_sample_actual)
+split_times = c(time[seq(1, length(time), by = 5)])
+split_times = head(split_times, -1)
+time <- factor(japan_time_series$time[34:74], levels = split_times)
+
 ggplot() +
-  geom_line(aes(x = time, y = difference, color = "Differences", group = 1))+
-  labs(title = "Time Series Plot of Differences - Canada/US Exchange rate", x = "Time", y = "Percentage Change") +
+  geom_line(aes(x = time, y = difference, group = 1)) +
+  labs(title = "Time Series Plot of Differences - Japan/US Exchange", x = "Time", y = "Percentage Difference") +
   theme_minimal() +
-  theme(text = element_text(size = 14),
-      legend.position = "bottom") 
-  #coord_cartesian(ylim = c(-20, 20))
-
-
+  theme(text = element_text(size = 14), legend.position = "bottom") +
+  coord_cartesian(ylim = c(-2.0, 2.0))
